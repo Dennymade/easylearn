b333247b50c450dbdc41930c55b422997ee4a0c8
Dennymade <129514436+Dennymade@users.noreply.github.com>
2023-03-31 13:51:21 +0100

    Add files via upload

---
+++ pasthos1.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="index.css">
+<style>
+h1 {
+  color: blue;
+  font-family: verdana;
+  font-size: 200%;
+}
+p  {
+  color: red;
+  font-family: courier;
+  font-size: 100%;
+}
+</style>
+</head>
+<body bgcolor="#f9dcb5">
+
+<header class="head1">
+<div class="header">
+  <header id:bghead>
+        <table bgcolor="#fffff0" style="border: none;">
+          <tr>
+            <td ><img src="image/nigerialogo5.jpg" width="70px" height="70px"></td>
+            <td  width="2000px" height="7px" >
+              
+                <center>
+                  <font color="purple" size="18"><bold> Ondo State Office of  Establishments and Training</bold></font>
+                </center>
+                
+            </td> 
+            <td  float="right"><img src="image/ondologo12.jpg" width="85px" height="80px"></td>
+          </tr>
+        </table>
+      </header>
+</div>
+</header>
+
+
+<ul class="toplink">
+  <li class="toplink"><a href="index.php" class="toplink">Home</a></li>
+  <li class="toplink"><a href="estabhis1.html" target="_blank" class="toplink">About Us</a></li>
+  <li class="toplink"><a href="#home" class="toplink">Gallery</a></li>
+  <li class="toplink"><a href="#news" class="toplink">News</a></li>
+  <li class="toplink"><a href="code/signin.php" class="toplink">Sign In</a></li>
+  <li class="toplink"><a href="#news" class="toplink">e-Services</a></li>
+  <li class="toplink"><a href="pasthos1.php" target="_blank" class="toplink">HOS office</a></li>
+  <li class="toplink"><a href="#news" class="toplink">PS office</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Departments</a>
+    <div class="dropdown-content">
+      <a href="#">Estabs </a>
+      <a href="#">Training</a>
+      <a href="#">ILR</a>
+    </div>
+  </li>
+</ul>
+
+<h2> "Meet The Present and Past HOS of Ondo State"</h2>
+<hr>
+
+
+<div class="row1">
+  <div class="column1"><img src="image/Ogundele.jpg" width="350px" height="300px" >
+  <P>Name:Kayode Ogundele Date:________________________________<br>
+  Comment:__________________________________<br></div>
+  <div class="column1"><img src="image/adeyemo1.jpg"  width="350px" height="300px"> <P>Name:Adeniran Adeyemo Date:________________________________<br>
+  Comment:__________________________________<br></div>
+  <div class="column1"><img src="image/aragbaye1.jpg" width="350px" height="300px"><P>Name:Dare Aragbaye Date:________________________________<br>
+  Comment:__________________________________<br></div>
+</div>
+
+<hr>
+<div class="row1">
+  <div class="column1"><img src="image/Akinkuotu.jpg" width="350px" height="300px"><P><font color=red>Name:</font> &nbsp;&nbsp;&nbsp;&nbsp;Barr. Toyin Akinkuotu Date:________________________________<br>
+  Comment:__________________________________<br></div>
+  <div class="column1"><img src="image/Kosemani.jpg"  width="350px" height="300px"><P>Name:Kofoworola Kosemani Date:________________________________<br>
+  Comment:__________________________________<br> </div>
+  <div class="column1"><img src="image/kudehinbu.jpg" width="350px" height="300px"><P>Name:Ajose Kudehinbu Date:________________________________<br>
+  Comment:__________________________________<br></div>
+</div>
+
+
+<hr>
+
+
+<footer class="footer">
+    
+      <div class="foot1">
+        
+        <font color="green"><b>Mission:&nbsp;</b> </font>
+
+        <font color="red">To organise a professionally strategised work-force  that is enviable, evenly distributed, stoically disciplined, and dogmatic in efficient service delivery </font>
+      </div>
+
+     <div class="foot2">
+      
+        <font color="green"><b>Vision:&nbsp;</b> </font>
+      <font color="red"> To build a resilient workforce  who will systematically engineer  the government's objective of  creating an egalitarian society where the goodwill of all citizens is highly prioritized.</font>
+        
+    
+      </div>
+    
+  </footer>
+
+</body>
+</html>
